84836dad69033b8698c908637c9591316dc65017
Yana Hanieh <yana.hanieh@neu.edu.tr>
2026-09-06 22:13:23 +0300

    feat: UI improvement

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ from answer import answer_query
 st.title("Local RAG Assistant")
 st.caption("Offline Q&A powdered by Microsoft's Foundry Local")
 
-question = st.text_input("Ask a question:")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if question:
-    answer_placeholder = st.empty()
-    with st.spinner("Thinking... Please wait..."):
-        answer = answer_query(question)
-    st.write(answer)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if question := st.chat_input("Ask a question"):
+    st.session_state.messages.append({"role": "user", "content": question})
+    with st.chat_message("User"):
+        st.write(question)
+
+    with st.chat_message("Assistant"):
+        with st.spinner("Thinking... Please wait..."):
+            answer = answer_query(question)
+        st.write(answer)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
